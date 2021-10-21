371268cfada08d12cc8271ebb1681ee341d1ffc2
Adrian <adriantutuianu01@gmail.com>
2021-10-21 22:58:30 +0100

    List in React

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import "./index.css";
 ReactDOM.render(
   // React.createElement("h1", null, "Hello!"),
   React.createElement(
-    "div",
+    "ul",
     { style: { color: "blue" } },
-    React.createElement("h1", null, "Hi!")
+    React.createElement("li", null, "hot dogs"),
+    React.createElement("li", null, "hamburger"),
+    React.createElement("li", null, "pizza")
   ),
   document.getElementById("root")
 );
