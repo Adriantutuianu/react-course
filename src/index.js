886b7384a11bb69fcbaaea478190b5d00008e817
Adrian <adriantutuianu01@gmail.com>
2021-10-22 22:34:06 +0100

    Library React in an object

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Hello() {
+function Hello(props) {
+  console.log(props);
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to {props.library}!</h1>
       <p>Let`s build something cool.</p>
     </div>
   );
@@ -13,6 +14,6 @@ function Hello() {
 
 ReactDOM.render(
   // React.createElement("h1", null, "Hello!"),
-  <Hello></Hello>,
+  <Hello library="React" />,
   document.getElementById("root")
 );
