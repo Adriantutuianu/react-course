f7780019925788d990a09b07414f0832d6218e79
Adrian <adriantutuianu01@gmail.com>
2021-10-23 23:54:24 +0100

    Using multiple different state variables

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App() {
+  const [manager, setManager] = useState("Alex");
   const [status, setStatus] = useState("Open");
   return (
-    <div>
-      <h1>Status: {status}</h1>
-      <button onClick={() => setStatus(" Open")}>Open</button>
-      <button onClick={() => setStatus(" Back in 5")}>Back in 5</button>
-      <button onClick={() => setStatus(" Closed")}>Closed</button>
-    </div>
+    <>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+      </div>
+      <div>
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus(" Open")}>Open</button>
+        <button onClick={() => setStatus(" Back in 5")}>Back in 5</button>
+        <button onClick={() => setStatus(" Closed")}>Closed</button>
+      </div>
+    </>
   );
 }
 
