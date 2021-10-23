ea4302b62b0e2098c4435bac9166c5363db4cde8
Adrian <adriantutuianu01@gmail.com>
2021-10-23 23:47:36 +0100

    UseState - Open-Closed

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Lake() {
-  return <h1>Lake</h1>;
-}
-
-function SkiResort() {
-  return <h1>Ski Resort</h1>;
-}
-
 function App() {
+  const [status, setStatus] = useState("Open");
   return (
-    <React.Fragment>
-      <Lake />
-      <SkiResort />
-    </React.Fragment>
+    <div>
+      <h1>Status: {status}</h1>
+      <button onClick={() => setStatus(" Closed")}>Closed</button>
+    </div>
   );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-const [first, second, third] = ["popcorn", "pretzel", "pineapple"];
-
-console.log(first);
-console.log(second);
-console.log(third);
