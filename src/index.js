5a5ec581f7791e6f10030e7f2fcf717e0396a21c
Adrian <adriantutuianu01@gmail.com>
2021-10-24 15:27:12 +0100

    Fetching data with useEffect

--- src/index.js
+++ src/index.js
@@ -2,31 +2,22 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
+  }
+  return null;
+}
 function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
-  useEffect(() => {
-    console.log(`field 1: ${val}`);
-  }, [val]);
-
-  useEffect(() => {
-    console.log(`field 2: ${val2}`);
-  }, [val2]);
-
-  return (
-    <>
-      <label>
-        Favorite Phrase:
-        <input value={val} onChange={(e) => setVal(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Second Favorite Phrase:
-        <input value={val2} onChange={(e) => setVal2(e.target.value)} />
-      </label>
-    </>
-  );
+  return <GitHubUser login="moonhighway" />;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
