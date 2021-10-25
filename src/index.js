1fc67aa61b8854b2c03c7f2fc3c43d3e05db1ab2
Adrian <adriantutuianu01@gmail.com>
2021-10-26 00:19:55 +0100

    Starting course React Essentials Linkedin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React, { useReducer } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-function Checkbox() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
-
-  return (
-    <>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      {checked ? "checked" : "not checked"}
-    </>
-  );
-}
-ReactDOM.render(<Checkbox />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
