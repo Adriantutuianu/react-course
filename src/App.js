110cb9be1b2320f665af3398fbe6925289a9381c
Adrian <adriantutuianu01@gmail.com>
2021-10-27 23:53:08 +0100

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
 import "./App.css";
-import restaurant from "./restaurant.jpg";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}`s Kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>;
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <img
-        src={restaurant}
-        height={200}
-        alt="napkin and fork at a restaurant table"
-      />
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-        ;
-      </ul>
-    </section>
-  );
+function Regularcomponent() {
+  return <h1>Everyone can see this component.</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = ["Macaroni and Cheese", "Salmon", "Tofu with veggie"];
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-console.log(dishObjects);
-function App() {
-  return (
-    <>
-      <Header name="Cindy" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </>
-  );
+function App(props) {
+  if (props.authorized) {
+    return <SecretComponent />;
+  } else {
+    return <Regularcomponent />;
+  }
 }
 
 export default App;
