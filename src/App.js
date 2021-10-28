6196d96e32880131d33851c6d875c58fd1c02dad
Adrian <adriantutuianu01@gmail.com>
2021-10-29 00:24:22 +0100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [checked, setChecked] = useState(false);
 
+  function toggle() {
+    setChecked((checked) => !checked);
+  }
+
   return (
     <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
+      <input type="checkbox" value={checked} onChange={toggle} />
       <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
