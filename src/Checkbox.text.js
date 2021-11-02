e464a6091fd150714bc1ffa0edb1a56f35479c7b
Adrian <adriantutuianu01@gmail.com>
2021-11-02 16:45:03 +0000

    Test getByLabelText

--- src/Checkbox.text.js
+++ src/Checkbox.text.js
@@ -1,1 +1,10 @@
-test("Selecting Checkbox", () => {});
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { Checkbox } from "./checkbox";
+
+test("Selecting Checkbox", () => {
+  const { getByLabelText } = render(<Checkbox />);
+  const checkbox = getByLabelText(/not checked/);
+  fireEvent.click(checkbox);
+  expect(checkbox.checked).toEqual(true);
+});
